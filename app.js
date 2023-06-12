5a370a02c29faa97f558a9cc6ae4cdbae0b80aba
FlavienJovic <flavien.jovic@gmail.com>
2023-06-12 11:35:47 +0200

    express 4

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", userHandlers.updateUser);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", userHandlers.postUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
